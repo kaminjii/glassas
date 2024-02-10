66d4ab4e9ed9047fcfe23755bc0594f6a94e1dc0
kaminjii <wood.kaitlin3@gmail.com>
2024-02-10 17:10:38 -0600

    youch

--- glasses/src/Game.js
+++ glasses/src/Game.js
@@ -2,29 +2,26 @@ import * as PIXI from "pixi.js";
 
 const app = new PIXI.Application({ resizeTo: window });
 
-const characterList=[]
-class character extends sprite{
-  #velocity={x:0,y:0}
-  #direction={x:0,y:0}
-  constructor(){
+const characterList = [];
+class character extends sprite {
+  #velocity = { x: 0, y: 0 };
+  #direction = { x: 0, y: 0 };
+  constructor() {
     characterList.push(this);
   }
 }
 
-const MAX_DIST=5;
-const checkCollision=(a,b)=>{
-  const dist=(a.x-b.x)**2+(a.y-b.y)**2;
-  return dist<MAX_DIST;
+const MAX_DIST = 5;
+const checkCollision = (a, b) => {
+  const dist = (a.x - b.x) ** 2 + (a.y - b.y) ** 2;
+  return dist < MAX_DIST;
 };
 
-app.ticker.add(()=>{
-  for(let i=0;i<characterList.length-1;i++)
-    for(let j=i+1;j<characterList.length;j++)
-      if(checkCollision(i,j))
-        resolveCollision(i,j);
-  
-})
-
+app.ticker.add(() => {
+  for (let i = 0; i < characterList.length - 1; i++)
+    for (let j = i + 1; j < characterList.length; j++)
+      if (checkCollision(i, j)) resolveCollision(i, j);
+});
 
 const canvas = app.view;
 export { canvas };
