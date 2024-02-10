7acb269a256a13b8deffffed06c6675d933b685b
Erregea5 <blastbaja15@gmail.com>
2024-02-10 17:04:38 -0600

    started collision detection

--- glasses/src/Game.js
+++ glasses/src/Game.js
@@ -2,77 +2,29 @@ import * as PIXI from "pixi.js";
 
 const app = new PIXI.Application({ resizeTo: window });
 
-// create a texture from an image path
-const texture = PIXI.Texture.from("https://pixijs.com/assets/bunny.png");
-
-// Scale mode for pixelation
-texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
-
-for (let i = 0; i < 10; i++) {
-  createBunny(
-    Math.floor(Math.random() * app.screen.width),
-    Math.floor(Math.random() * app.screen.height)
-  );
-}
-
-function createBunny(x, y) {
-  // create our little bunny friend..
-  const bunny = new PIXI.Sprite(texture);
-
-  // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-  bunny.eventMode = "static";
-
-  // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-  bunny.cursor = "pointer";
-
-  // center the bunny's anchor point
-  bunny.anchor.set(0.5);
-
-  // make it a bit bigger, so it's easier to grab
-  bunny.scale.set(3);
-
-  // setup events for mouse + touch using
-  // the pointer events
-  bunny.on("pointerdown", onDragStart, bunny);
-
-  // move the sprite to its designated position
-  bunny.x = x;
-  bunny.y = y;
-
-  // add it to the stage
-  app.stage.addChild(bunny);
-}
-
-let dragTarget = null;
-
-app.stage.eventMode = "static";
-app.stage.hitArea = app.screen;
-app.stage.on("pointerup", onDragEnd);
-app.stage.on("pointerupoutside", onDragEnd);
-
-function onDragMove(event) {
-  if (dragTarget) {
-    dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+const characterList=[]
+class character extends sprite{
+  #velocity={x:0,y:0}
+  #direction={x:0,y:0}
+  constructor(){
+    characterList.push(this);
   }
 }
 
-function onDragStart() {
-  // store a reference to the data
-  // the reason for this is because of multitouch
-  // we want to track the movement of this particular touch
-  // this.data = event.data;
-  this.alpha = 0.5;
-  dragTarget = this;
-  app.stage.on("pointermove", onDragMove);
-}
+const MAX_DIST=5;
+const checkCollision=(a,b)=>{
+  const dist=(a.x-b.x)**2+(a.y-b.y)**2;
+  return dist<MAX_DIST;
+};
 
-function onDragEnd() {
-  if (dragTarget) {
-    app.stage.off("pointermove", onDragMove);
-    dragTarget.alpha = 1;
-    dragTarget = null;
-  }
-}
+app.ticker.add(()=>{
+  for(let i=0;i<characterList.length-1;i++)
+    for(let j=i+1;j<characterList.length;j++)
+      if(checkCollision(i,j))
+        resolveCollision(i,j);
+  
+})
+
 
 const canvas = app.view;
 export { canvas };
