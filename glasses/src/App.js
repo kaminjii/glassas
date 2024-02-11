48aaa4df14881e2789580a48c47dab1d6e2676ae
Diana <diananguyen0424@gmail.com>
2024-02-11 00:33:30 -0600

    restructure

--- glasses/src/App.js
+++ glasses/src/App.js
@@ -1,29 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import backgroundImage from './startmenu_background2.png';
-import startButton from './start_button2.png';
-import movingCharacter from './moving_character2.png';
-import titleImage from './title.png';
+import backgroundImage from './start_menu_imgs/start_menu_background.png';
+import startButton from './start_menu_imgs/start_button.png';
+import movingCharacter from './start_menu_imgs/moving_character.png';
+import titleImage from './start_menu_imgs/title.png';
 
 
 function App() {
   return (
-      <main className=" w-screen h-screen" style={{ 
-        width: '100vw', 
-        height: '100vh', 
-        backgroundImage: `url(${backgroundImage})`, 
-        backgroundRepeat: 'no-repeat', 
-        backgroundSize: 'cover'}}>
-        <section className="center-content">
-          <audio autoPlay loop>
-            <source src="your-music-file.mp3" type="audio/mpeg" />
-          </audio>
-          <img src={titleImage} alt="Title Image" className="title-image" width="50%" />
-          <img src={startButton} alt="Start Button" className="start-button" width="25%" onClick={() => console.log('Start Game')} />
-          <img src={process.env.PUBLIC_URL + '/moving_character.png'} alt="" className="moving-character" />
-          <img src={movingCharacter} alt="Moving Character" className="moving-character" />
-        </section>
-      </main>
+    <main className=" w-screen h-screen" style={{ 
+      width: '100vw', 
+      height: '100vh', 
+      backgroundImage: `url(${backgroundImage})`, 
+      backgroundRepeat: 'no-repeat', 
+      backgroundSize: 'cover'}}>
+      <section className="center-content">
+        <img src={titleImage} alt="Title Image" className="title-image" width="50%" />
+        <img src={startButton} alt="Start Button" className="start-button" width="25%" onClick={() => console.log('Start Game')} />
+        <img src={process.env.PUBLIC_URL + '/moving_character.png'} alt="" className="moving-character" />
+        <img src={movingCharacter} alt="Moving Character" className="moving-character" />
+      </section>
+    </main>
   );
 }
 
