a5bf3f29c32df9e5eda015514da82d65d885b1ce
kaminjii <wood.kaitlin3@gmail.com>
2024-02-11 02:11:51 -0600

    Edit Pushing mechanic

--- glasses/src/Create.js
+++ glasses/src/Create.js
@@ -2,18 +2,17 @@ import * as PIXI from "pixi.js";
 import { setupFrames } from "./functions/functions";
 const app = new PIXI.Application({
   background: "#1099bb",
-  resizeTo: window
+  resizeTo: window,
 });
 
-const characterArray=[];
-const glassesArray=[];
-const wallArray=[];
+const characterArray = [];
+const glassesArray = [];
+const wallArray = [];
 
-const create={};
-create.sprite=(x,y,scale,still,animationSpeed,src,arr,callback)=>{
+const create = {};
+create.sprite = (x, y, scale, still, animationSpeed, src, arr, callback) => {
   let sprite;
-  if(still)
-    sprite=PIXI.Sprite.from(src);
+  if (still) sprite = PIXI.Sprite.from(src);
   else
     sprite = new PIXI.AnimatedSprite(
       src.map((frame) => PIXI.Texture.from(frame))
@@ -24,74 +23,106 @@ create.sprite=(x,y,scale,still,animationSpeed,src,arr,callback)=>{
   sprite.scale.x = scale;
   sprite.scale.y = scale;
   sprite.anchor.set(0.5);
-  if(!still){
+  if (!still) {
     sprite.animationSpeed = animationSpeed;
     sprite.play();
   }
-  if(callback)
-    callback(sprite);
-  if(arr)
-    arr.push(sprite);
-  console.log('adding',sprite)
+  if (callback) callback(sprite);
+  if (arr) arr.push(sprite);
+  console.log("adding", sprite);
   app.stage.addChild(sprite);
   return sprite;
 };
-create.character=(x,y,scale,still,animationSpeed,src,type)=>create.sprite(x,y,scale,still,animationSpeed,src,characterArray,sprite=>{
-  sprite.velocity = {x: 0, y: 0};
-  sprite.type = type;
-  sprite.equip = function (glasses) {
-    this.glasses = glasses;
-    app.stage.removeChild(glasses);
-    alert(glasses.dialog);
-  };
-  sprite.shoot=function(){
-    let xComponent = this.velocity.x;
-    let yComponent = this.velocity.y;
-    const fireball = create.character(
-      this.x,
-      this.y,
-      0.25,
-      false,
-      .1,
-      setupFrames("assets/actual/fireball", 3)
-    );
+create.character = (x, y, scale, still, animationSpeed, src, type) =>
+  create.sprite(
+    x,
+    y,
+    scale,
+    still,
+    animationSpeed,
+    src,
+    characterArray,
+    (sprite) => {
+      sprite.velocity = { x: 0, y: 0 };
+      sprite.type = type;
+      sprite.equip = function (glasses) {
+        this.glasses = glasses;
+        app.stage.removeChild(glasses);
+        alert(glasses.dialog);
+      };
+      sprite.shoot = function () {
+        let xComponent = this.velocity.x;
+        let yComponent = this.velocity.y;
+        const fireball = create.character(
+          this.x,
+          this.y,
+          0.25,
+          false,
+          0.1,
+          setupFrames("assets/actual/fireball", 3)
+        );
 
-    // Turn it into unit vectors first
-    let magnitude = Math.sqrt(xComponent ** 2 + yComponent ** 2);
-    xComponent /= magnitude;
-    yComponent /= -magnitude;
+        // Turn it into unit vectors first
+        let magnitude = Math.sqrt(xComponent ** 2 + yComponent ** 2);
+        xComponent /= magnitude;
+        yComponent /= -magnitude;
 
-    const moveFireball = (delta) => {
-      if (
-        fireball.x < 0 ||
-        fireball.x > app.screen.width ||
-        fireball.y < 0 ||
-        fireball.y > app.screen.height
-      ) {
-        app.stage.removeChild(fireball);
-        app.ticker.remove(moveFireball);
-        return;
-      }
-      let SPEED = 12;
-      fireball.x += xComponent * SPEED;
-      fireball.y += yComponent * SPEED;
-    };
+        const moveFireball = (delta) => {
+          if (
+            fireball.x < 0 ||
+            fireball.x > app.screen.width ||
+            fireball.y < 0 ||
+            fireball.y > app.screen.height
+          ) {
+            app.stage.removeChild(fireball);
+            app.ticker.remove(moveFireball);
+            return;
+          }
+          let SPEED = 12;
+          fireball.x += xComponent * SPEED;
+          fireball.y += yComponent * SPEED;
+        };
 
-    app.ticker.add(moveFireball);
-  };
-});
+        app.ticker.add(moveFireball);
+      };
+    }
+  );
 
 create.glasses = (x, y, scale, glasses) => {
-  return create.sprite(x, y, scale, true, 0, glasses.path, glassesArray, (sprite) => {
-    sprite.name = glasses.name;
-    sprite.dialog = glasses.dialog;
-  });
+  return create.sprite(
+    x,
+    y,
+    scale,
+    true,
+    0,
+    glasses.path,
+    glassesArray,
+    (sprite) => {
+      sprite.name = glasses.name;
+      sprite.dialog = glasses.dialog;
+    }
+  );
 };
 
-create.walls=(x,y,scale,still,animationSpeed,src,type)=>create.sprite(x,y,scale,still,animationSpeed,src,wallArray,(sprite)=>{
-  sprite.type=type
-});
-
+create.walls = (x, y, scale, still, animationSpeed, src, type) =>
+  create.sprite(
+    x,
+    y,
+    scale,
+    still,
+    animationSpeed,
+    src,
+    wallArray,
+    (sprite) => {
+      sprite.type = type;
+    }
+  );
 
 // FIXME
-export {create,characterArray as characters,wallArray as walls,glassesArray as glasses,app};
+export {
+  create,
+  characterArray as characters,
+  wallArray as walls,
+  glassesArray as glasses,
+  app,
+};
--- glasses/src/Kaitlin.js
+++ glasses/src/Kaitlin.js
@@ -2,19 +2,19 @@ import * as PIXI from "pixi.js";
 
 const app = new PIXI.Application({ background: "#e0e0e0", resizeTo: window });
 
-function setup(sprite, character, speed){
-  function followCharacter (delta) {
+function setup(sprite, character, speed) {
+  function followCharacter(delta) {
     const dx = character.x - sprite.x;
     const dy = character.y - sprite.y;
-    
+
     const distance = Math.sqrt(dx * dx + dy * dy);
-    
+
     const directionX = dx / distance;
     const directionY = dy / distance;
-    
+
     sprite.x += directionX * speed * delta;
     sprite.y += directionY * speed * delta;
-  };
+  }
 
   const activateFollow = () => {
     app.ticker.add(followCharacter);
@@ -22,40 +22,10 @@ function setup(sprite, character, speed){
 
   const deactivateFollow = () => {
     app.ticker.remove(followCharacter);
-  }
+  };
 
-  return {activateFollow, deactivateFollow}
-};
-
-function getXEdges(object) {
-  return [object.x - object.width / 2, object.x + object.width / 2];
+  return { activateFollow, deactivateFollow };
 }
-
-function getYEdges(object) {
-  return [object.y - object.height / 2, object.y + object.height / 2];
-}
-
-function distBetweenEdge(sprite, character) {
-  let spriteEdgeX = getXEdges(sprite);
-  let spriteEdgesY = getYEdges(sprite);
-
-  let characterEdgeX = getXEdges(character);
-  let characterEdgesY = getYEdges(character);
-
-}
-
-const moveSprite(sprite, character) => {
-  app.ticker.add(() => {
-    const dx = character.x - sprite.x;
-    const dy = character.y - sprite.y;
-      
-    const distance = Math.sqrt(dx * dx + dy * dy);
-
-    if (distance < 5){
-
-    };
-  })
-};
 
 const canvas = app.view;
 export { canvas };
